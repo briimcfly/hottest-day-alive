82db1d62bbfff7c3512b8f57719bf6f020f3693d
Bryan Bickel <briimcfly@gmail.com>
2023-07-23 15:36:34 -0700

    fetching city data from API on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=0040c54f9bc97c402ce34384520d63ae`)
-.then(function (response) {
-    return response.json();
-}) 
-.then(function (data){
-    console.log(data);
-})
+const APIKey = '0040c54f9bc97c402ce34384520d63ae';
+
+function findCity(){
+    var city = document.querySelector(`input`).value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    
+    fetch (queryURL)
+    .then(function (response) {
+        return response.json();
+    }) 
+    .then(function (data){
+        console.log(data);
+    })
+}
